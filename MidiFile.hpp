1c490c2cf83c63f05aa73c0e1d2d925ce4ba741b
Mohamed Abdel Maksoud <mohamed_am83@yahoo.com>
2015-02-11 13:29:59 +0100

    adding license text

--- MidiFile.hpp
+++ MidiFile.hpp
@@ -3,9 +3,19 @@
 
 /**
 * Name:        MidiFile.hpp
-* Purpose:     C++ re-write of the python module MidiFile.py
+* Purpose:     C++ re-write of (parts of) the functionality provided by 
+*              the python midutil module (https://code.google.com/p/midiutil/)
+*              by Mark Conway Wirt.
 * Author:      Mohamed Abdel Maksoud <mohamed at amaksoud.com>
 * Created:     2015/02/11
+* License:
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 *-----------------------------------------------------------------------------
 */
 
